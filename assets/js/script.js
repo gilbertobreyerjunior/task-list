fd78f200c0066845dfe74ecd6bc8daf75f3033e4
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2021-01-05 13:45:09 -0300

    Realizando testes com JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,34 @@
-let data = [];
+let data = [{
 
+
+id: 1,
+title: "Estudar Node"
+
+
+
+},
+{
+
+id: 2,
+title: "Estudar HTML5"
+
+},
+{
+id: 3,
+title: "Estudar JavaScript"
+
+
+},
+
+{
+
+id: 4,
+title: "Estudar Laravel"
+
+
+}
+
+];
+
+
+console.log(data);
